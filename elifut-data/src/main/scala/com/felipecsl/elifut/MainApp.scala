978b30447b5b259787d241185f5cbc2d96d84ab1
Felipe Lima <felipecsl@stripe.com>
2019-09-22 22:57:34 -0500

    MainApp works!

--- elifut-data/src/main/scala/com/felipecsl/elifut/MainApp.scala
+++ elifut-data/src/main/scala/com/felipecsl/elifut/MainApp.scala
@@ -1,18 +1,22 @@
 package com.felipecsl.elifut
 
-import scala.util.Success
+import java.util.concurrent.TimeUnit
+
+import akka.pattern.{ask, pipe}
 import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.unmarshalling.Unmarshal
-import akka.pattern.pipe
 import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
+import akka.util.Timeout
 import spray.json.RootJsonFormat
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
+import scala.util.{Failure, Success}
 
 class ItemsResponseParsingActor extends Actor with ActorLogging {
+
   import com.felipecsl.elifut.PlayerJsonProtocol._
   import context.dispatcher
 
@@ -22,19 +26,20 @@ class ItemsResponseParsingActor extends Actor with ActorLogging {
   private implicit val itemsJsonFormat: RootJsonFormat[ItemsResponse] = jsonFormat6(ItemsResponse)
 
   def receive: PartialFunction[Any, Unit] = {
-    case resp @ HttpResponse(StatusCodes.OK, _, _, _) =>
+    case resp@HttpResponse(StatusCodes.OK, _, _, _) =>
       val origSender = sender()
       Unmarshal(resp).to[ItemsResponse].onComplete(r => {
         origSender ! r.get
       })
-    case resp @ HttpResponse(code, _, _, _) =>
+    case resp@HttpResponse(code, _, _, _) =>
       log.info("Request failed, response code: " + code)
       resp.discardEntityBytes()
   }
 }
 
 class ItemsRequestingActor(
-    sendAndReceive: HttpRequest => Future[HttpResponse]
+    sendAndReceive: HttpRequest => Future[HttpResponse],
+    implicit val ec: ExecutionContext
 ) extends Actor with ActorLogging {
 
   private val baseUrl = "https://www.easports.com/fifa/ultimate-team/api/fut/item"
@@ -42,14 +47,26 @@ class ItemsRequestingActor(
   def receive: PartialFunction[Any, Unit] = {
     case page =>
       val uri = baseUrl + "?jsonParamObject=%7B\"page\":" + page + "%7D"
-      sender() ! sendAndReceive(HttpRequest(uri = uri))
+      val s = sender()
+      sendAndReceive(HttpRequest(uri = uri)).onComplete {
+        case Success(response) => s ! response
+        case Failure(exception) => s ! exception
+      }
   }
 }
 
 object MainApp extends App {
-//  implicit val system: ActorSystem = ActorSystem()
-//  private val clazz = classOf[ItemsResponseParsingActor]
-//  private val requestToFuture = (r: HttpRequest) => Http(system).singleRequest(r)
-//  private val actorRef: ActorRef = system.actorOf(Props(clazz, 1, requestToFuture), "requester")
-//  actorRef ! "ignored"
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val timeout: Timeout = Timeout(5, TimeUnit.SECONDS)
+  implicit val dispatcher: ExecutionContextExecutor = system.dispatcher
+  private val parsingActorClass = classOf[ItemsResponseParsingActor]
+  private val requestingActorClass = classOf[ItemsRequestingActor]
+  private val requestToFuture = (r: HttpRequest) => Http(system).singleRequest(r)
+  private val requesterProps = Props(requestingActorClass, requestToFuture, dispatcher)
+  private val requestingActorRef = system.actorOf(requesterProps, "requester")
+  private val parsingActorRef: ActorRef = system.actorOf(Props(parsingActorClass), "parser")
+  private val responseFuture = requestingActorRef.ask(1).mapTo[HttpResponse]
+  responseFuture.flatMap(parsingActorRef.ask)
+      .mapTo[ItemsResponse]
+      .onComplete(r => println(r))
 }
--- elifut-data/src/test/scala/com/felipecsl/elifut/ItemsResponseParsingActorSpec.scala
+++ elifut-data/src/test/scala/com/felipecsl/elifut/ItemsResponseParsingActorSpec.scala
@@ -1,32 +1,24 @@
 package com.felipecsl.elifut
 
 import akka.actor.{ActorSystem, Props}
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpRequest, HttpResponse, StatusCodes}
-import akka.http.scaladsl.util.FastFuture
-import akka.testkit.{ImplicitSender, TestActors, TestKit, TestProbe}
+import akka.http.scaladsl.model._
+import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
+import scala.concurrent.Future
 import scala.io.Source
 
-class ItemsResponseParsingActorSpec (_system: ActorSystem)
+class ItemsResponseParsingActorSpec(_system: ActorSystem)
     extends TestKit(_system)
-    with Matchers
-    with ImplicitSender
-    with WordSpecLike
-    with BeforeAndAfterAll {
+        with Matchers
+        with ImplicitSender
+        with WordSpecLike
+        with BeforeAndAfterAll {
 
   def this() = this(ActorSystem("ItemRootRequestActorSpec"))
 
   override def afterAll: Unit = {
     shutdown(system)
-  }
-
-  "An Echo actor" must {
-    "send back messages unchanged" in {
-      val echo = system.actorOf(TestActors.echoActorProps)
-      echo ! "hello world"
-      expectMsg("hello world")
-    }
   }
 
   "An ItemsResponseParsingActor" must {
@@ -41,6 +33,22 @@ class ItemsResponseParsingActorSpec (_system: ActorSystem)
       val response = expectMsgType[ItemsResponse]
       response.totalPages should ===(908)
       response.totalResults should ===(21791)
+    }
+  }
+
+  "An ItemsRequestingActor" must {
+    "make the HTTP request" in {
+      val json = """{"hello":"world"}"""
+      val response: HttpRequest => Future[HttpResponse] =
+        _ => Future.successful {
+          HttpResponse(
+            status = StatusCodes.OK,
+            entity = HttpEntity(ContentTypes.`application/json`, json))
+        }
+      val props = Props(classOf[ItemsRequestingActor], response, system.dispatcher)
+      val actor = system.actorOf(props, "foo")
+      actor ! "1"
+      expectMsgType[HttpResponse]
     }
   }
 }
