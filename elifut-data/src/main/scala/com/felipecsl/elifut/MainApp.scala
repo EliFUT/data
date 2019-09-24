daeb53bff14095b47b5f43c4b35ea2e98c82489e
Felipe Lima <felipe.lima@gmail.com>
2019-09-23 19:52:43 -0500

    better tests

--- elifut-data/src/main/scala/com/felipecsl/elifut/MainApp.scala
+++ elifut-data/src/main/scala/com/felipecsl/elifut/MainApp.scala
@@ -8,17 +8,18 @@ import akka.http.scaladsl.model.HttpRequest
 import com.felipecsl.elifut.actors.{ItemsFetchingActor, PlayersFetchingActor}
 import akka.pattern.ask
 import akka.util.Timeout
+import com.felipecsl.elifut.models.Player
 
 import scala.concurrent.ExecutionContext
 
 object MainApp extends App {
-  implicit val system: ActorSystem = ActorSystem()
+  implicit private val system: ActorSystem = ActorSystem()
   implicit private val timeout: Timeout = Timeout(5, TimeUnit.SECONDS)
-  implicit val dispatcher: ExecutionContext = system.dispatcher
+  implicit private val dispatcher: ExecutionContext = system.dispatcher
   private val requestToFuture = (r: HttpRequest) => Http(system).singleRequest(r)
   private val props = Props(classOf[PlayersFetchingActor], requestToFuture, timeout)
   private val itemsFetchingActor = system.actorOf(props, "fetcher")
   itemsFetchingActor.ask()
-      .mapTo[ItemsResponse]
+      .mapTo[Seq[Player]]
       .onComplete(println)
 }
--- elifut-data/src/test/scala/com/felipecsl/elifut/PlayersFetchingActorSpec.scala
+++ elifut-data/src/test/scala/com/felipecsl/elifut/PlayersFetchingActorSpec.scala
@@ -3,10 +3,10 @@ package com.felipecsl.elifut
 import java.util.concurrent.TimeUnit
 
 import akka.actor.{ActorSystem, Props}
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpRequest, HttpResponse, StatusCodes}
+import akka.http.scaladsl.model._
 import akka.testkit.{ImplicitSender, TestKit}
 import akka.util.Timeout
-import com.felipecsl.elifut.actors.{ItemsFetchingActor, PlayersFetchingActor}
+import com.felipecsl.elifut.actors.PlayersFetchingActor
 import com.felipecsl.elifut.models.Player
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
@@ -29,23 +29,27 @@ class PlayersFetchingActorSpec(_system: ActorSystem)
   "An PlayersFetchingActor" must {
     "fetch all item pages" in {
       val timeout = Timeout(5, TimeUnit.SECONDS)
-      // TODO: Return a different JSON for each page request so we can really test the pagination logic
-      val json = Source.fromResource("item.json").mkString
+      val json = Source.fromResource("singleItemWithNamePlaceholder.json").mkString
       val responseFn: HttpRequest => Future[HttpResponse] =
-        _ => Future.successful {
+        request => Future.successful {
+          val uri = request.uri.toString()
+          // Use the page number from the URL as the player name
+          val finalJson = json.replace(
+            "<PLAYER NAME PLACEHOLDER>",
+            s"Player ${uri.substring(uri.indexOf("\":") + 2)}".replace("%7D", "")
+          )
           HttpResponse(
             status = StatusCodes.OK,
-            entity = HttpEntity(ContentTypes.`application/json`, json))
+            entity = HttpEntity(ContentTypes.`application/json`, finalJson))
         }
       val props = Props(classOf[PlayersFetchingActor], responseFn, timeout)
       val actor = system.actorOf(props, "playerFetcher")
       actor ! "fetch"
       val response = expectMsgType[Seq[Player]]
-      response.size should ===(24 * 908) // items per page * total pages
-      response.map(_.name).toSet should ===(Set(
-        "Reus", "Cristiano Ronaldo", "Lewandowski", "Pelé", "Sergio Ramos", "Suárez", "Maradona",
-        "Hazard", "Neymar Jr", "Messi", "Modrić"
-      ))
+      response.size should ===(10)
+      response.map(_.name).sorted should ===(List(
+        "Player 1", "Player 10", "Player 2", "Player 3", "Player 4", "Player 5", "Player 6",
+        "Player 7", "Player 8", "Player 9"))
     }
   }
 }
